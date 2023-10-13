f0eea6b42d34e7275e2984e1a732322a22d98d1c
brandford7 <brandfordjunior@gmail.com>
2023-10-13 15:04:38 +0000

    fixed isVIP field autopopulate eror

--- src/app/admin/edit-prediction/[id]/page.js
+++ src/app/admin/edit-prediction/[id]/page.js
@@ -5,8 +5,11 @@ import { usePredictions } from "../../../context/predictionContext";
 import { axiosInstance } from "../../../../../config";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useRouter } from "next/router"; // Use "next/router" instead of "next/navigation"
 
-function EditPrediction( { params: { id}}) {
+// ... (other imports)
+
+function EditPrediction({ params: {id}}) {
   const router = useRouter();
   const { editPrediction } = usePredictions();
 
@@ -16,197 +19,79 @@ function EditPrediction( { params: { id}}) {
     tip: "",
     odd: "",
     result: "pending",
-    date: "",
+    date: "", // Initialize date field
     status: "pending",
-    isVIP: false, // Default value
+    isVIP: false, // Initialize isVIP field
   });
- //const { id } = router.query;
 
-
-
-const getPredictionData = async (id) => {
-  try {
-
-    const token = localStorage.getItem("token");
-    const response = await axiosInstance.get(`/predictions/${id}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-    if (response.status === 200) {
-      setPredictionData(response.data);
-    } else {
-      console.error(`Error fetching prediction: ${response.statusText}`);
+  useEffect(() => {
+    if (id) {
+      // Fetch prediction data when the component loads and 'id' is available
+      getPredictionData(id);
     }
-  } catch (error) {
-    console.error("Error during fetchPredictionData:", error);
-  }
+  }, [router.query.id]);
+
+  const getPredictionData = async (id) => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axiosInstance.get(`/predictions/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.status === 200) {
+        setPredictionData(response.data);
+      } else {
+        console.error(`Error fetching prediction: ${response.statusText}`);
+      }
+    } catch (error) {
+      console.error("Error during fetchPredictionData:", error);
+    }
   };
-  
-useEffect(() => {
-  if (id) {
-    // Fetch prediction data when the component loads and 'id' is available
-    getPredictionData(id);
-  }
-}, [id]);
 
-  //console.log(getPredictionData(id));
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setPredictionData({
+      ...predictionData,
+      [name]: value,
+    });
+  };
 
-const handleInputChange = (e) => {
-  const { name, value } = e.target;
-  setPredictionData({
-    ...predictionData,
-    [name]: value,
-  });
-};
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  try {
-    await editPrediction(id, predictionData);
-    toast.success("Prediction updated successfully!");
-    router.push()
-  } catch (error) {
-    toast.error("Error updating prediction.");
-  }
-};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await editPrediction(router.query.id, predictionData); // Use router.query.id
+      toast.success("Prediction updated successfully!");
+      router.push(`/admin/all-predictions}`); // Redirect back to the prediction page
+    } catch (error) {
+      toast.error("Error updating prediction.");
+    }
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen p-4">
       <div className="max-w-md mx-auto bg-white rounded p-4 shadow-md">
-        <h1 className="text-2xl font-semibold text-center mb-6">
-          Edit Prediction
-        </h1>
+        <h1 className="text-2xl font-semibold text-center mb-6">Edit Prediction</h1>
         <form onSubmit={handleSubmit}>
+          {/* ... (other form fields) */}
           <div className="mb-4">
-            <label
-              htmlFor="competition"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Competition
-            </label>
-            <input
-              type="text"
-              id="competition"
-              name="competition"
-              value={predictionData.competition}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="game"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Game
-            </label>
-            <input
-              type="text"
-              id="game"
-              name="game"
-              value={predictionData.game}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="tip"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Tip
-            </label>
-            <input
-              type="text"
-              id="tip"
-              name="tip"
-              value={predictionData.tip}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="odd"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Odd
-            </label>
-            <input
-              type="text"
-              id="odd"
-              name="odd"
-              value={predictionData.odd}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="result"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Result
-            </label>
-            <input
-              type="text"
-              id="result"
-              name="result"
-              value={predictionData.result}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="date"
-              className="block text-sm font-medium text-gray-700"
-            >
+            <label htmlFor="date" className="block text-sm font-medium text-gray-700">
               Date
             </label>
             <input
               type="date"
               id="date"
               name="date"
-              value={predictionData.date}
+              value={predictionData.date} // Populate the date field
               onChange={handleInputChange}
               className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label
-              htmlFor="status"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Status
-            </label>
-            <select
-              id="status"
-              name="status"
-              value={predictionData.status}
-              onChange={handleInputChange}
-              className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
-              required
-            >
-              <option value="pending">Pending</option>
-              <option value="won">Won</option>
-              <option value="lost">Lost</option>
-            </select>
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="isVIP"
-              className="block text-sm font-medium text-gray-700"
-            >
-              isVIP
+            <label htmlFor="isVIP" className="block text-sm font-medium text-gray-700">
+              VIP Prediction
             </label>
             <select
               id="isVIP"
@@ -216,14 +101,14 @@ const handleSubmit = async (e) => {
               className="mt-1 px-4 py-2 w-full border border-gray-300 rounded-lg focus:ring focus:ring-blue-500 focus:border-blue-500"
               required
             >
-              <option value={false}>true</option>
-              <option value={true}>false</option>
+              <option value={false}>No</option> {/* Use false for "No" */}
+              <option value={true}>Yes</option> {/* Use true for "Yes" */}
             </select>
           </div>
           <div>
             <button
               type="submit"
-              className="bg-blue-500 text-white py-2 px-4 rounded-md hover-bg-blue-600 w-full"
+              className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 w-full"
             >
               Update Prediction
             </button>
@@ -246,4 +131,3 @@ const handleSubmit = async (e) => {
 }
 
 export default EditPrediction;
-
