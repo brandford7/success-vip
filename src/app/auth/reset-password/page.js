cdda67bc95f7ec09c12f551547cd667647526414
brandford7 <brandfordjunior@gmail.com>
2023-10-22 13:57:40 +0000

    fixed import error in rest password page

--- src/app/auth/reset-password/page.js
+++ src/app/auth/reset-password/page.js
@@ -1,7 +1,7 @@
-import { useAuth } from "./authContext";
+import { useAuth } from "../../context/authContext";
 import { ToastContainer, toast } from "react-toastify";
 
-const ResetPassword=()=> {
+const ResetPassword = () => {
   const { forgotPassword } = useAuth();
   const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -19,13 +19,13 @@ const ResetPassword=()=> {
         autoClose: 2000, // Close the notification after 3 seconds
       });
     } else {
-        // Handle failure
-         toast.error("Password reset request failed. Please try again.", {
-           position: "top-right",
-           autoClose: 3000, // Close the notification after 3 seconds
-         });
+      // Handle failure
+      toast.error("Password reset request failed. Please try again.", {
+        position: "top-right",
+        autoClose: 3000, // Close the notification after 3 seconds
+      });
     }
-  }
+  };
 
   return (
     <div>
@@ -51,6 +51,6 @@ const ResetPassword=()=> {
       />
     </div>
   );
-}
+};
 
 export default ResetPassword;
