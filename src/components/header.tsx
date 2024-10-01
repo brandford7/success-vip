04e736f225a2cabdac7edcabca399b3909790eab
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-01 19:30:11 +0000

    removed unused components

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className=" flex px-3 gap-3 items-center lg:justify-center bg-primary  py-5 md:py-10 mb-10">
       <h1 className="capitalize">Success Secrets Bet</h1>
-      <Image
-        src="/success-image.webp"
-        alt="logo"
-        height="30"
-        width="30"
-        className="rounded-full"
-      />
+      <Link href="/">
+        <Image
+          src="/success-image.webp"
+          alt="logo"
+          height="30"
+          width="30"
+          className="rounded-full"
+        />
+      </Link>
     </header>
   );
 };
