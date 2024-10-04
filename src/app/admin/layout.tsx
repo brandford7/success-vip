baeb730a56ac247ca33570219787831d8219a13a
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-04 09:58:13 +0000

    changed overflow to horizontal

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -13,7 +13,7 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
   };
 
   return (
-    <div className="admin overflow-hidden ">
+    <div className="admin overflow-x-hidden ">
       <Head>
         <title>Admin</title>
         <meta name="description" content="Admin Page" />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <title>Success Secrets Bet</title>
         <meta name="description" content="The Home Of Betting Tips" />
       </head>
-      <body className={`${inter.className}flex flex-col gap-y-10 min-h-screen overflow-hidden`} >
+      <body className={`${inter.className}flex flex-col gap-y-10 min-h-screen overflow-x-hidden`} >
         {/* adsense script */}
         {/*
         <Script
