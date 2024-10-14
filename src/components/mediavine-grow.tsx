d8a93e2cc00cddaa196fdbf0cfaffb97a1b4877e
Brandford Junior Korang-Teku <Brandfordko@stlghana.com>
2024-10-14 15:56:07 +0000

    changed ezoic grow script

--- src/components/mediavine-grow.tsx
+++ src/components/mediavine-grow.tsx
@@ -7,8 +7,7 @@ const GrowScript = () => {
       strategy="lazyOnload"
       data-grow-initializer=""
       dangerouslySetInnerHTML={{
-        __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTphZGQzM2EyZS00OWJjLTQzMzctOGNkOS03NGIxY2M4ZDg2ZTM=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
-      }}
+        __html:`!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo1MjVhZWIwMy03M2U0LTQyYWUtYTc4Ny03ZmIyNzgwYmE5Mjg=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`   }}
     />
   );
 };
